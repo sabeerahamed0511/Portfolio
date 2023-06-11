4785e7d5e75cd5b0356f26db443ad6d23631455f
sabeerahamed0511 <sabeerahamed0511@gmail.com>
2023-06-11 21:14:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Project from './components/Project';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <About />
+      <Project />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
